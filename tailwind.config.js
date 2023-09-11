bc613a38381b44b095dc4214033f7d1f045125ea
Yhucee14 <uchechukwu7814@gmail.com>
2023-09-11 04:07:31 -0700

    setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "white",
+        secondary: "#63b543",
+        other: "#347d58",
+        dimBlue: "#193e40",
+        dimother: "#f0f5f5",
+        lightGreen: "#d5f5d6",
+        greenGradient: "bg-gradient-to-r from-cyan-500 to-blue-500",
+      },
+
+      fontFamily: {
+        poppins: ["Quicksand", "sans-serif"],
+      },
+    },
+    screens: {
+      xx: "320px",
+      xs: "412px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+}

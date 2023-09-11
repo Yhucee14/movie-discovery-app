3aa11752b287d5a382718bb4897829fce8b5e1c4
Yhucee14 <uchechukwu7814@gmail.com>
2023-09-11 09:46:23 -0700

    trying to consume API

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import { useState,useEffect } from "react"
+import Hero from "../src/components/Hero"
 
-
+const API_URL= "https://api.themoviedb.org/3/movie/popular?api_key=328b25fcc08fbc96577be0d1fee70666";
 const App = () => {
+
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+   fetch(API_URL)
+   .then((res)=>res.json())
+   .then(data=>{
+    console.log(data)
+    setMovies(data.results);
+   })
+  }, [])
+  
+
   return (
-    <div>App</div>
+    <div>
+      {movies.map((movieReq)=>
+      <Hero key={movieReq.id} {...movieReq} />)}
+    </div>
   )
 }
 
